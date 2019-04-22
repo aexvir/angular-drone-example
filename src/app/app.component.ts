89392c1df11a0e9a50ed7cf924bb56f7f36cc756
Matthias Alt <mattagohni@gmail.com>
2019-04-22 23:27:21 +0200

    generated products-list.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,23 @@ import {Product} from './product.model';
 export class AppComponent {
   title = 'inventory-app';
   @Input() products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        '435-ret',
+        'Graphics Card Deluxe',
+        'http://images.test/graphics/card/deluxe.jpg',
+        ['Graphics', 'Accessories', 'Deluxe'],
+        567.78
+      ),
+      new Product(
+        '398-beast',
+        'The HD Beast',
+        'http://images.test/graphics/card/beast.jpg',
+        ['Graphics', 'Accessories', 'Performance'],
+        567.78
+      ),
+    ];
+  }
 }
