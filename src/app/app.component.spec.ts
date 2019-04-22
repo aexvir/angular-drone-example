a0568bcf0bd167a8f5217a2d3b9a4b83861d2752
Matthias Alt <mattagohni@gmail.com>
2019-04-22 23:20:19 +0200

    added a products array to the app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {Product} from './product.model';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +14,42 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    app.products = [
+      new Product(
+        '435-ret',
+        'Graphics Card Deluxe',
+        'http://images.test/graphics/card/deluxe.jpg',
+        ['Graphics', 'Accessories', 'Deluxe'],
+        567.78
+      ),
+      new Product(
+        '398-beast',
+        'The HD Beast',
+        'http://images.test/graphics/card/beast.jpg',
+        ['Graphics', 'Accessories', 'Performance'],
+        567.78
+      ),
+    ];
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'inventory-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('inventory-app');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to inventory-app!');
+  });
+
+  it('should have products', () => {
+    expect(app.products).toBeDefined('no products found');
   });
 });
