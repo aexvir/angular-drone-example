be960558a7eb31dde190d76d7f404bff9da93293
Matthias Alt <mattagohni@gmail.com>
2019-05-06 20:53:28 +0200

    add product to row component before each test

--- src/app/product-row/product-row.component.spec.ts
+++ src/app/product-row/product-row.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductRowComponent } from './product-row.component';
+import {ProductImageComponent} from '../product-image/product-image.component';
+import {Product} from '../product.model';
 
 describe('ProductRowComponent', () => {
   let component: ProductRowComponent;
@@ -8,7 +10,7 @@ describe('ProductRowComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductRowComponent ]
+      declarations: [ ProductRowComponent]
     })
     .compileComponents();
   }));
@@ -16,6 +18,14 @@ describe('ProductRowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductRowComponent);
     component = fixture.componentInstance;
+    component.product = new Product(
+      '435-ret',
+      'Graphics Card Deluxe',
+      'http://images.test/graphics/card/deluxe.jpg',
+      ['Graphics', 'Accessories', 'Deluxe'],
+      567.78
+    );
+
     fixture.detectChanges();
   });
 
