92fe2b0f237b3eed33e943a6b72b9c53a1d4fab6
Matthias Alt <mattagohni@gmail.com>
2019-05-11 13:13:23 +0200

    use extra config for ci

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -3,8 +3,10 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   'chromeOptions': {
-    'args': ['no-sandbox']
+    args: ['--headless', '--no-sandbox']
   }
 };
 
+config.seleniumAddress = 'http://chrome:4444/wd/hub';
+config.directConnect = false;
 exports.config = config;
