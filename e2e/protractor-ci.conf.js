021f4616847672141214cb29719f3cb3f1da7f31
Matthias Alt <mattagohni@gmail.com>
2019-05-11 13:21:58 +0200

    accept insecure cert during tests

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -4,7 +4,8 @@ config.capabilities = {
   browserName: 'chrome',
   'chromeOptions': {
     args: ['--headless', '--no-sandbox']
-  }
+  },
+  'acceptInsecureCerts': true
 };
 
 config.seleniumAddress = 'http://chrome:4444/wd/hub';
